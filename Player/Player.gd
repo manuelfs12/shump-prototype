fb237aa59fc8f36d582e263ca59a74930a09f4b2
Manuel Figueroa <manuelfigueroa402@gmail.com>
2021-03-30 06:13:09 -0400

    change animation from animated sprite to animation tree

--- Player/Player.gd
+++ Player/Player.gd
@@ -5,37 +5,27 @@ const MAX_SPEED = 80
 const FRICTION = 500
 
 var velocity = Vector2.ZERO
-onready var animatedSprite = $AnimatedSprite
+
+onready var animationTree = $AnimationTree
+onready var animationState = animationTree.get("parameters/playback")
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
-	var mouse_position = get_local_mouse_position()
+	var mouse_position = get_local_mouse_position().normalized()
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
 	
+	animationTree.set("parameters/Idle/blend_position", mouse_position)
 	if input_vector != Vector2.ZERO:
-		if mouse_position.y < -20 and mouse_position.x > 0:
-			animatedSprite.play("RunUpRight")
-		elif mouse_position.y < -20 and mouse_position.x < 0:
-			animatedSprite.play("RunUpLeft")
-		elif mouse_position.x > 0:
-			animatedSprite.play("RunRight")
-		elif mouse_position.x < 0:
-			animatedSprite.play("RunLeft")
+		animationTree.set("parameters/Run/blend_position", mouse_position)
+		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 		
 	else:
-		if mouse_position.y < -20 and mouse_position.x > 0:
-			animatedSprite.play("IdleUpRight")
-		elif mouse_position.y < -20 and mouse_position.x < 0:
-			animatedSprite.play("IdleUpLeft")
-		elif mouse_position.x > 0:
-			animatedSprite.play("IdleRight")
-		elif mouse_position.x < 0:
-			animatedSprite.play("IdleLeft")
+		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-#	print(velocity)
-	print(mouse_position)
+	print(velocity)
+#	print(mouse_position)
 	velocity = move_and_slide(velocity)
