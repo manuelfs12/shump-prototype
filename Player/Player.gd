2e650adf40cd2874044763ee70601e9ce81959a7
Manuel Figueroa <manuelfigueroa402@gmail.com>
2021-03-31 01:01:23 -0400

    add player weapon

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,6 +8,7 @@ var velocity = Vector2.ZERO
 
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
+onready var waterGun = $WaterGun
 
 func _physics_process(delta):
 	var input_vector = Vector2.ZERO
@@ -26,6 +27,16 @@ func _physics_process(delta):
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-	print(velocity)
+#	print(velocity)
 #	print(mouse_position)
+#	print(rad2deg(mouse_position.angle()))
 	velocity = move_and_slide(velocity)
+	gun_rotation(mouse_position)
+	
+	
+func gun_rotation(mouse_position):
+	waterGun.rotation_degrees = rad2deg(mouse_position.angle())
+	if waterGun.rotation_degrees > 90 or waterGun.rotation_degrees < -90:
+		waterGun.get_node("Sprite").flip_v = true
+	else:
+		waterGun.get_node("Sprite").flip_v = false
